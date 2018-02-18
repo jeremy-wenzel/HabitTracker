2ed762f20749f98a3089757e7e8624c8d8be69de
Jeremy Wenzel <wenzeljeremy15@gmail.com>
2018-02-17 21:13:04 -0500

    Created the daily habit and have it showing

--- app/src/main/java/me/jwenzel/habittracker/MainActivity.java
+++ app/src/main/java/me/jwenzel/habittracker/MainActivity.java
@@ -1,52 +1,51 @@
 package me.jwenzel.habittracker;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ArrayList<String> testArr = new ArrayList<String>();
+        testArr.add("test1");
+        testArr.add("test2");
+        testArr.add("test3");
+        ListView list = (ListView) findViewById(R.id.main_list_view);
+        CustomAdapter arrayAdapter = new CustomAdapter(this, testArr);
+        list.setAdapter(arrayAdapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public class CustomAdapter extends ArrayAdapter<String> {
+        public CustomAdapter(Context context, ArrayList<String> input) {
+            super(context, android.R.layout.simple_list_item_1, input);
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            String str = getItem(position);
+            if (convertView == null) {
+                Log.d("TestTag", "The convert view is null");
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.row_daily_habit, parent, false);
+            }
+
+            TextView tvHabitName = (TextView) convertView.findViewById(R.id.daily_title);
+            tvHabitName.setText(str);
+            return convertView;
+        }
     }
 }
